d43994b658d48b39866d76c1c3072d252661faef
camalzade_elvin <camalzade_elvin@outlook.com>
2019-01-28 00:10:31 +0400

    I added country to combobox . . .

--- Team Manager/Form1.Designer.cs
+++ Team Manager/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.labelCountry = new System.Windows.Forms.Label();
             this.metroCmCountry = new MetroFramework.Controls.MetroComboBox();
+            this.pictureFlag = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureFlag)).BeginInit();
             this.SuspendLayout();
             // 
             // labelCountry
@@ -50,16 +52,29 @@
             this.metroCmCountry.Name = "metroCmCountry";
             this.metroCmCountry.Size = new System.Drawing.Size(192, 29);
             this.metroCmCountry.TabIndex = 1;
+            this.metroCmCountry.SelectedIndexChanged += new System.EventHandler(this.metroCmCountry_SelectedIndexChanged);
+            // 
+            // pictureFlag
+            // 
+            this.pictureFlag.Location = new System.Drawing.Point(99, 87);
+            this.pictureFlag.Name = "pictureFlag";
+            this.pictureFlag.Size = new System.Drawing.Size(124, 64);
+            this.pictureFlag.TabIndex = 2;
+            this.pictureFlag.TabStop = false;
+            this.pictureFlag.Click += new System.EventHandler(this.pictureFlag_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(481, 473);
+            this.Controls.Add(this.pictureFlag);
             this.Controls.Add(this.metroCmCountry);
             this.Controls.Add(this.labelCountry);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureFlag)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -69,6 +84,7 @@
 
         private System.Windows.Forms.Label labelCountry;
         private MetroFramework.Controls.MetroComboBox metroCmCountry;
+        private System.Windows.Forms.PictureBox pictureFlag;
     }
 }
 
--- Team Manager/Form1.cs
+++ Team Manager/Form1.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,41 @@ namespace Team_Manager
         public Form1()
         {
             InitializeComponent();
+            metroCmCountry.DataSource = GetCountries();
+        }
+        private List<string> GetCountries()
+        {
+            List<string> CulturesInfo = new List<string>();
+            CultureInfo[] cultures = CultureInfo.GetCultures(CultureTypes.SpecificCultures);
+            foreach (CultureInfo culture in cultures)
+            {
+                RegionInfo info = new RegionInfo(culture.LCID);
+                if (!CulturesInfo.Contains(info.EnglishName))
+                {
+                    CulturesInfo.Add(info.EnglishName);
+                }
+            }
+            CulturesInfo.Sort();
+            return CulturesInfo;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureFlag_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroCmCountry_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            
+            String url = "https://www.countryflags.io/az/flat/64.png";
+            pictureFlag.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureFlag.Load(url);
         }
     }
 }
