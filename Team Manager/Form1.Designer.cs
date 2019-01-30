5af5b40ff5c67badf3a41a78d6ddbb624ab9d701
camalzade_elvin <camalzade_elvin@outlook.com>
2019-01-30 12:51:53 +0400

    I added some colors

--- Team Manager/Form1.Designer.cs
+++ Team Manager/Form1.Designer.cs
@@ -30,7 +30,6 @@
         {
             this.labelCountry = new System.Windows.Forms.Label();
             this.metroCmCountry = new MetroFramework.Controls.MetroComboBox();
-            this.pictureFlag = new System.Windows.Forms.PictureBox();
             this.labelPosition = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -38,39 +37,33 @@
             this.metroCbPosition = new MetroFramework.Controls.MetroComboBox();
             this.buttonGenerate = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureFlag)).BeginInit();
             this.groupBoxTopLabels.SuspendLayout();
             this.SuspendLayout();
             // 
             // labelCountry
             // 
             this.labelCountry.AutoSize = true;
-            this.labelCountry.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCountry.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.labelCountry.Location = new System.Drawing.Point(12, 9);
             this.labelCountry.Name = "labelCountry";
-            this.labelCountry.Size = new System.Drawing.Size(64, 20);
+            this.labelCountry.Size = new System.Drawing.Size(75, 24);
             this.labelCountry.TabIndex = 0;
             this.labelCountry.Text = "Country";
             // 
             // metroCmCountry
             // 
+            this.metroCmCountry.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(255)))), ((int)(((byte)(226)))));
             this.metroCmCountry.FormattingEnabled = true;
             this.metroCmCountry.ItemHeight = 23;
             this.metroCmCountry.Location = new System.Drawing.Point(109, 4);
             this.metroCmCountry.Name = "metroCmCountry";
             this.metroCmCountry.Size = new System.Drawing.Size(192, 29);
             this.metroCmCountry.TabIndex = 1;
+            this.metroCmCountry.UseCustomBackColor = true;
+            this.metroCmCountry.UseCustomForeColor = true;
             this.metroCmCountry.UseSelectable = true;
+            this.metroCmCountry.UseStyleColors = true;
             this.metroCmCountry.SelectedIndexChanged += new System.EventHandler(this.metroCmCountry_SelectedIndexChanged);
-            // 
-            // pictureFlag
-            // 
-            this.pictureFlag.Location = new System.Drawing.Point(337, 5);
-            this.pictureFlag.Name = "pictureFlag";
-            this.pictureFlag.Size = new System.Drawing.Size(72, 28);
-            this.pictureFlag.TabIndex = 2;
-            this.pictureFlag.TabStop = false;
-            this.pictureFlag.Click += new System.EventHandler(this.pictureFlag_Click);
             // 
             // labelPosition
             // 
@@ -112,22 +105,26 @@
             // 
             // metroCbPosition
             // 
+            this.metroCbPosition.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(255)))), ((int)(((byte)(226)))));
             this.metroCbPosition.FormattingEnabled = true;
             this.metroCbPosition.ItemHeight = 23;
-            this.metroCbPosition.Location = new System.Drawing.Point(441, 5);
+            this.metroCbPosition.Location = new System.Drawing.Point(328, 3);
             this.metroCbPosition.Name = "metroCbPosition";
-            this.metroCbPosition.Size = new System.Drawing.Size(61, 29);
+            this.metroCbPosition.Size = new System.Drawing.Size(125, 29);
             this.metroCbPosition.TabIndex = 5;
+            this.metroCbPosition.UseCustomBackColor = true;
+            this.metroCbPosition.UseCustomForeColor = true;
             this.metroCbPosition.UseSelectable = true;
+            this.metroCbPosition.UseStyleColors = true;
             this.metroCbPosition.SelectedIndexChanged += new System.EventHandler(this.metroComboBox1_SelectedIndexChanged);
             // 
             // buttonGenerate
             // 
-            this.buttonGenerate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.buttonGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.buttonGenerate.Location = new System.Drawing.Point(371, 546);
+            this.buttonGenerate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(255)))), ((int)(((byte)(226)))));
+            this.buttonGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonGenerate.Location = new System.Drawing.Point(381, 557);
             this.buttonGenerate.Name = "buttonGenerate";
-            this.buttonGenerate.Size = new System.Drawing.Size(121, 40);
+            this.buttonGenerate.Size = new System.Drawing.Size(121, 54);
             this.buttonGenerate.TabIndex = 6;
             this.buttonGenerate.Text = "Generate";
             this.buttonGenerate.UseVisualStyleBackColor = false;
@@ -148,19 +145,18 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(144)))), ((int)(((byte)(255)))), ((int)(((byte)(220)))));
             this.ClientSize = new System.Drawing.Size(539, 654);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.buttonGenerate);
             this.Controls.Add(this.metroCbPosition);
             this.Controls.Add(this.groupBoxTopLabels);
-            this.Controls.Add(this.pictureFlag);
             this.Controls.Add(this.metroCmCountry);
             this.Controls.Add(this.labelCountry);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureFlag)).EndInit();
             this.groupBoxTopLabels.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -171,7 +167,6 @@
 
         private System.Windows.Forms.Label labelCountry;
         private MetroFramework.Controls.MetroComboBox metroCmCountry;
-        private System.Windows.Forms.PictureBox pictureFlag;
         private System.Windows.Forms.Label labelPosition;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
